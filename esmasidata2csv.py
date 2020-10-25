8466617d9feda9cb2e7554d5ee520a0e1bfbe206
ron c project <ron.c.project@gmail.com>
2020-10-25 11:17:06 +0100

    improved logging

--- esmasidata2csv.py
+++ esmasidata2csv.py
@@ -3,19 +3,22 @@
 import logging
 import sys
 from openpyxl import load_workbook
- 
+
+
+# Get the workbook filename from the commandline
 def get_arg():
     logging.debug("esmasidata2csv.get_arg() start")
     try:
         arg = sys.argv[1]
     except IndexError:
-        logging.warning("esmasidata2csv.get_arg() IndexError")
+        logging.warning("esmasidata2csv.get_arg() WARNING: IndexError")
         raise SystemExit(f"Usage: {sys.argv[0]} <esma data file>")
 
     return arg
     logging.debug("esmasidata2csv.get_arg() end")
 
 
+#Load the workbook and write the list
 def get_list(fname):
     logging.debug("esmasidata2csv.fname() start")
 
@@ -28,65 +31,76 @@ def get_list(fname):
                 si_list.append(value)
         return si_list
     except:
-        logging.warning("esmasidata2csv.get_arg() An exception occurred while reading the workbook")
-        raise SystemExit("An exception occurred while reading the workbook")
+        logging.critical("esmasidata2csv.get_arg() CRITICAL: An exception occurred while reading the workbook")
+        raise SystemExit("CRITICAL: could not read the workbook, app exits")
 
     logging.debug("esmasidata2csv.fname() end")
 
 
+# Print the csv from the list
 def print_list(list):
     logging.debug("esmasidata2csv.print_list() start")
 
-    # First 5 values are titles, discard
-    list.pop(0)
-    list.pop(0)
-    list.pop(0)
-    list.pop(0)
-    list.pop(0)
+    try:
+        # First 5 values are titles, discard
+        list.pop(0)
+        list.pop(0)
+        list.pop(0)
+        list.pop(0)
+        list.pop(0)
 
-    length = len(list)
+        length = len(list)
 
-    i = 0
-    j = 0
-    lines = 0
+        i = 0
+        j = 0
+        lines = 0
 
-    # Loop loop loop
-    while i < length:
-        if j == 0:
-            print(list[i], ",", end='')
-        if j == 1:
-            print(list[i], ",", end='')
-        if j == 2:
-            print(list[i], ",", end='')
-        if j == 3:
-            print(list[i], ",", end='')
-        if j == 4:
-            print(list[i], ",", end='')
-            print("")  # newline
-            lines += 1
-        i += 1
-        j += 1
-        if j == 5:
-            j = 0
+        # Loop through the list and print
+        while i < length:
+            if j == 0:
+                print(list[i], ",", end='')
+            if j == 1:
+                print(list[i], ",", end='')
+            if j == 2:
+                print(list[i], ",", end='')
+            if j == 3:
+                print(list[i], ",", end='')
+            if j == 4:
+                print(list[i], ",", end='')
+                print("")  # newline
+                lines += 1
+            i += 1
+            j += 1
+            if j == 5:
+                j = 0
+
+    except:
+        logging.error("esmasidata2csv.print_list() ERROR: An error occurred while writing the list")
+        raise SystemExit("ERROR: could not write the csv, app exits")
+
     logging.debug("esmasidata2csv.print_list() end")
-    return  lines
+    return lines
 
+
+# Main procedure
 def main():
     logging.basicConfig(
         filename="esmasidata2csv.log",
         format='%(asctime)s %(levelname)-8s %(message)s',
-        level=logging.INFO,
-        datefmt='%Y-%m-%d %H:%M:%S')
+        level=logging.DEBUG,     # Possibilities: NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
     logging.debug("esmasidata2csv.main() start")
 
     f = get_arg()
-    #print("File: ", f)
     list = get_list(f)
-    #print("List: ", list)
     l = print_list (list)
     logging.debug("esmasidata2csv.main() end")
     logging.info("%s %s", "esmasidata2csv. number of lines: ", l)
 
-# Press the green button in the gutter to run the script.
+    logging.debug("esmasidata2csv.main() end")
+
+
+# Script start point
 if __name__ == '__main__':
     main()
